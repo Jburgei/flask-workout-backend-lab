7508a6f5de1bc47b097007c73b8321728ab18647
Jburgei <joyburgei4@gmail.com>
2026-04-16 09:27:42 +0300

    Set up Flask app with database configuration and test route

---
+++ server/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify, make_response
+from flask_migrate import Migrate
+from models import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db' 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+migrate = Migrate(app, db)
+#testing route
+@app.route('/')
+def home():
+    return {"message": "Workout API is running!"}, 200
+
+if __name__ == '__main__':
+    app.run(port =5555, debug=True)
