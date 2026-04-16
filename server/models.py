27d9a0613e050c45190baceae5d65ca612efb167
Jburgei <joyburgei4@gmail.com>
2026-04-16 10:26:43 +0300

    Create Workout model with constraints and validations

--- server/models.py
+++ server/models.py
@@ -1,6 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
 from sqlalchemy import CheckConstraint
+from datetime import datetime
+
+
 db = SQLAlchemy()
 
 
@@ -32,4 +35,29 @@ class Exercise(db.Model):
         if value not in alowed_categories:
             raise ValueError(f"Category must be one of: {', '.join(allowed_categories)}.")
         return value
+
+
+class Workout(db.Model):
+    __tablename__ = 'workouts'
+
+    __table_args__ = (
+        CheckConstraint("duration_minutes > 0", name = "check_duration_positive"),
+    )
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    duration_minutes = db.Column(db.Integer, nullable=False)
+    notes = db.Column(db.Text)
+
+    @validates('duration_minutes')
+    def validate_duration_minutes(self, key, value):
+        if value is None or value <= 0:
+            raise ValueError("Workout duration must be greater than 0. ")
+            return value
+
         
+    @validates('date')
+    def validate_date(self, key, value):
+        if value > date.today():
+            raise ValueError("Workout date cannot be in the future.")
+            return value
